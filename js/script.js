1553660361216dff66d44b5ffad27890183ef348
Luke00117 <129889931+Luke00117@users.noreply.github.com>
2023-12-15 09:59:49 +0000

    test cookie storage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// This code is for the sliding banner
 document.addEventListener("DOMContentLoaded", function () {
     var currentSlide = 0;
     var totalSlides = document.querySelectorAll('.banner').length;
@@ -30,41 +32,74 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     window.addEventListener('resize', function () {
-      updateSlide(); // Update slide on window resize
+      updateSlide(); 
     });
 
-    // Automatically switch to the next slide every 5 seconds
+    
     setInterval(nextSlide, 5000);
 
-    // Manual navigation button click event listeners
+    
     document.querySelectorAll('.manual-btn').forEach(function (btn, index) {
       btn.addEventListener('click', function () {
         goToSlide(index);
       });
     });
 
-    // Initial setup
+    
     updateSlide();
   });
 
 
 
 
+
+
+//this function is for the hamburge button to togle the sidebar
+
 function toggleSidebar() {
-    var hamburger = document.querySelector('.hamburger');
-    hamburger.classList.toggle('active2');
+  var hamburger = document.querySelector('.hamburger');
+  hamburger.classList.toggle('active2');
 
-    var sidebar = document.querySelector('.side-menu');
-    sidebar.classList.toggle('active2');
+  var sidebar = document.querySelector('.side-menu');
+  sidebar.classList.toggle('active2');
 
-    const overlay = document.querySelector('#overlay');
-    overlay.classList.toggle('active2');
+  const overlay = document.querySelector('#overlay');
+  overlay.classList.toggle('active2');
 
-    const container = document.querySelector('#container');
+  const container = document.querySelector('#container');
 
-    if (sidebar.classList.contains('active2')) {
-        container.style.transform = 'translateX(-275px)';
-    } else {
-        container.style.transform = 'translateX(0)';
-    }
+ 
+  if (sidebar.classList.contains('active2')) {
+    container.style.transform = 'translateX(-275px)';
+    document.body.style.overflow = 'hidden'; 
+  } else {
+    container.style.transform = 'translateX(0)';
+    document.body.style.overflow = ''; 
+  }
 }
+
+
+
+
+// This code is for the cookie popup
+
+const cookieAlert = document.querySelector(".cookie-container"),
+  buttons = document.querySelectorAll(".cookie-btn");
+
+  const executeCookie = () => {
+    cookieAlert.classList.add("show");
+
+    buttons.forEach(button =>  {
+      button.addEventListener("click", () => {
+        cookieAlert.classList.remove("show");
+       
+        if(button.id == "accept-btn") {
+          document.cookie = "cookieBy= mrAnderson; max-age="+ 60 * 60 * 24 * 30;
+        }
+      })
+    })
+  }
+  
+
+// function will be called on page load  
+window.addEventListener("load", executeCookie)
